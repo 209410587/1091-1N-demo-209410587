897f82fa6181b98105abf67bfe23c58e86957d5d
209410587 <209410587@gms.tku.edu.tw>
2020-10-26 20:37:02 +0800

    Initial commit, saving w07 files

--- nav.js
+++ nav.js
@@ -3,27 +3,27 @@ function change(index) {
   console.log('show', p);
   switch (index) {
     case 1:
-      p.innerHTML = '<iframe width=100% height=100% scr="./w01/index.html" />';
+      p.innerHTML = '<iframe width=100% height=100% src="./w01/index.html" />';
       break;
     case 2:
       p.innerHTML =
-        '<iframe width=100% height=100% scr="./w01/pokemon.html" />';
+        '<iframe width=100% height=100% src="./w01/pokemon.html" />';
       break;
     case 3:
       p.innerHTML =
-        '<iframe width=100% height=100% scr="./w02-imgGallery/image.html" />';
+        '<iframe width=100% height=100% src="./w02-imgGallery/image.html" />';
       break;
     case 4:
       p.innerHTML =
-        '<iframe width=100% height=100% scr="./w02-blog/blog.html" />';
+        '<iframe width=100% height=100% src="./w02-blog/blog.html" />';
       break;
     case 5:
       p.innerHTML =
-        '<iframe width=100% height=100% scr="./w03-div/div.html" />';
+        '<iframe width=100% height=100% src="./w03-div/div.html" />';
       break;
     case 6:
       p.innerHTML =
-        '<iframe width=100% height=100% scr="./w04-tku60-js/div_tku60.html" />';
+        '<iframe width=100% height=100% src="./w04-tku60-js/div_tku60.html" />';
       break;
   }
 }
