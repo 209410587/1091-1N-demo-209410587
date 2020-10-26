8c430725b4ef76749c4a3fcd013aacf9ddb5253d
209410587 <209410587@gms.tku.edu.tw>
2020-10-26 20:16:16 +0800

    Initial commit, saving w07 files

---
+++ nav.js
@@ -0,0 +1,29 @@
+function change(index) {
+  const p = document.getElementById('show');
+  console.log('show', p);
+  switch (index) {
+    case 1:
+      p.innerHTML = '<iframe width=100% height=100% scr="./w01/index.html" />';
+      break;
+    case 2:
+      p.innerHTML =
+        '<iframe width=100% height=100% scr="./w01/pokemon.html" />';
+      break;
+    case 3:
+      p.innerHTML =
+        '<iframe width=100% height=100% scr="./w02-imgGallery/image.html" />';
+      break;
+    case 4:
+      p.innerHTML =
+        '<iframe width=100% height=100% scr="./w02-blog/blog.html" />';
+      break;
+    case 5:
+      p.innerHTML =
+        '<iframe width=100% height=100% scr="./w03-div/div.html" />';
+      break;
+    case 6:
+      p.innerHTML =
+        '<iframe width=100% height=100% scr="./w04-tku60-js/div_tku60.html" />';
+      break;
+  }
+}
